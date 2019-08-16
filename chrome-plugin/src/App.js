7e8d09c40336287cf02ee562a10f9db43c4cedfa
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-08-16 18:52:08 +0900

    Add Tab icon display

--- chrome-plugin/src/App.js
+++ chrome-plugin/src/App.js
@@ -1,27 +1,38 @@
 /*global chrome*/
 import React, {Component} from 'react';
 import QRCode from "qrcode.react";
-import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: []
+      url: [],
+      title: [],
+      favIconUrl: [],
+      // object_tab: []
     };
   }
   componentDidMount() {
-    const that = this;
+    // const that = this;
     chrome.tabs.getSelected(tab=>{
-      var temp = tab.url;
-      that.setState({url: temp});
+      const url_temp = tab.url;
+      const title_temp = tab.title;
+      const favIconUrl_temp = tab.favIconUrl;
+      // const object_tab_temp = tab.object_tab;
+      this.setState({url: url_temp});
+      this.setState({title: title_temp});
+      this.setState({favIconUrl: favIconUrl_temp});
+      // that.setState({title: object_tab_temp});
     })
   }
   render() {
     return(
         <div>
           <p>{this.state.url}</p>
+          <p>{this.state.title}</p>
           <QRCode value={this.state.url} />
+          <img src={this.state.favIconUrl} alt="icon"/>
+          {/*<p>{this.state.object_tab}</p>*/}
         </div>
     )
   }
